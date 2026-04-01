3bbcc979702b9f9bf65a203e77f086d1279c83a4
j8s0nn <23lequocthinh06@gmail.com>
2026-03-31 23:04:10 -0600

    Helper methods for World

--- GUI/Components/Models/World.cs
+++ GUI/Components/Models/World.cs
@@ -4,38 +4,111 @@ public class World
 {
     public Dictionary<int, Player> Players { get; private set; }
 
+    
     public Dictionary<int, Wall> Walls { get; private set; }
     
     public Dictionary<int, Powerup> Powerups { get; private set; }
 
-    private int Width { get; set; }
-    private int Height { get; set; }
+    private int Width { get; set; } = 0;
+    private int Height { get; set; } = 0;
 
-    // public World(int width, int height)
-    // {
-    //     
-    // }
+    public World()
+    {
+        Players = new Dictionary<int, Player>();
+        Walls = new Dictionary<int, Wall>();
+        Powerups = new Dictionary<int, Powerup>();
+        
+    }
     
 
     public void SetPlayerID(int ID)
     {
-        if (Players.ContainsKey(ID))
+            if (Players.ContainsKey(ID))
+            {
+                Players[ID] = new Player();
+            }
+            else
+            {
+                Players.Add(ID, new Player());
+            }
+        
+
+    
+    }
+
+    public void AddPlayer(Player player)
+    {
+        if (player == null)
         {
-            Players[ID] = new Player();
+            return;
+        }
+        
+        if (Players.ContainsKey(player.ID))
+        {
+            Players[player.ID] = player;
         }
         else
         {
-            Players.Add(ID, new Player());
+            Players.Add(player.ID, player);
         }
+
+        
+
     }
 
 
-
-    public void SetWorldSize(int width, int height)
+    public void SetSize(int width, int height)
     {
-        Width = width;
-        Height = height;
+        
+            Width = width;
+            Height = height;
+            
+        
+
     }
+
+    public void AddWalls(Wall wall)
+    {
+        if (wall == null)
+        {
+            return;
+        }
+
+        if (Walls.ContainsKey(wall.ID))
+        {
+            Walls[wall.ID] = wall;
+        }
+        else
+        {
+            Walls.Add(wall.ID, wall);
+        }
+            
+        
+
+
+    }
+
+    public void AddPowerup(Powerup powerup)
+    {
+        if (powerup == null)
+        {
+            return;
+        }
+
+        if (Powerups.ContainsKey(powerup.ID))
+        {
+            Powerups[powerup.ID] = powerup;
+        }
+        else
+        {
+            Powerups.Add(powerup.ID, powerup);
+        }
+            
+        
+
+    }
+
+
 
 
 }
